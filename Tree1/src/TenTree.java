6ed99ab3297e3b077ca28b102552ede652078d5a
Alexander Barnhill <arbl@methodpark.de>
2018-01-10 19:19:55 +0100

    Found a possible solution to the tree problem

--- Tree1/src/TenTree.java
+++ Tree1/src/TenTree.java
@@ -14,28 +14,56 @@ public class TenTree {
     }
 
     private static int longest(TenTree tree) {
-        int l = 0;
         if(height(tree) == 0) {
             return 0;
         }
 
+        //without root node
         int without = 0;
         for(TenTree t : tree.children) {
-            
+            if(t != null) {
+                int l = longest(t);
+                if(l >= without ) {
+                    without = l;
+                }
+            }
+
         }
 
+        //with root node
         int with = 0;
+        int max1 = 0;
+        int max2 = 0;
+        for(TenTree t : tree.children) {
+            if(t != null) {
+                int ll = height(t);
+                if(ll >= max1 && ll >= max2) {
+                    max1 = ll;
+                } else if(ll >= max2 && ll < max1) {
+                    max2 = ll;
+                }
+            }
+        }
+
+        with = max1 + max2 + 2;
+
+
         return Math.max(with, without);
     }
 
     public static void main(String[] args) {
         TenTree root = new TenTree();
         root.children[0] = new TenTree();
+        root.children[0].children[0] = new TenTree();
+        root.children[0].children[1] = new TenTree();
         root.children[0].children[2] = new TenTree();
-        root.children[0].children[2].children[7] = new TenTree();
-        root.children[1] = new TenTree();
-        root.children[2] = new TenTree();
+        root.children[0].children[0].children[0] = new TenTree();
+        root.children[0].children[0].children[0].children[0] = new TenTree();
 
-        System.out.println(longest(root));
+        root.children[1] = new TenTree();
+        root.children[1].children[0] = new TenTree();
+
+
+       System.out.println("Longest Path: " + longest(root));
     }
 }
